a9f33c932ba54162f706c95fa87930e4b28760af
Sergey Popinevskiy <gurza000@gmail.com>
2017-04-03 09:52:06 +0300

    Added connector to Sita WS

--- app/pnr/routes.py
+++ app/pnr/routes.py
@@ -1,11 +1,35 @@
 # -*- coding: utf-8 -*-
 from flask import render_template
 from . import pnr
+from lxml import etree
+from config import sita as sita_config
+from sitaclient import SitaClient
 
 
 @pnr.route('/pnr/<pnr_id>', methods=['GET'])
 def price_pnr(pnr_id):
+    sita = SitaClient(
+        pos=sita_config['pos'],
+        dumps_dir=sita_config['dumps_dir'] if 'dumps_dir' in sita_config else None
+    )
+    pnr_response = sita.read_pnr(pnr_id)
+    ns = {'common': ''}
+    if 'common' in pnr_response.nsmap:
+        ns['common'] = pnr_response.nsmap['common']
+    segments = pnr_response.findall('.//common:OriginDestinationOption', namespaces=ns)
+    itinerary = list()
+    for segment in segments:
+        itinerary.append({
+            'MarketingAirline': 'S7',
+            'FlightNumber': 99,
+            'DepartureAirport': 'OVB',
+            'ArrivalAirport': 'DME',
+            'DepartureDateTime': '2017-05-20T01:00:00',
+            'ArrivalDateTime': '2017-05-20T07:00:00',
+            'ResBookDesigCode': 'S',
+        })
     context = {
         'pnr_id': pnr_id,
+        'pnr': etree.tostring(pnr_response, pretty_print=True),
     }
     return render_template('pnr.html', **context)
--- config.py
+++ config.py
@@ -1,4 +1,26 @@
 # -*- coding: utf-8 -*-
+import os
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+sita = {
+    'pos': {
+        'gateway': 'https://sws.qa.sita.aero/sws/',
+        'certificate': '/'.join([cwd, 'sita_keys/cert.pem']),
+        'private_key': '/'.join([cwd, 'sita_keys/key.pem']),
+        'ERSP_UserID': 'ROV901/1ROV1AS',
+        'AgentSine': '4499/4499W',
+        'PseudoCityCode': 'ROV901',
+        'AgentDutyCode': '115',
+        'ISOCountry': 'RU',
+        'AirlineVendorID': 'S7',
+        'AirportCode': 'ROV',
+        'AXIRequestorID': 'xdfkd-9ejdo-ofkdo-slnc8',
+    },
+    'dumps_dir': '/tmp/sitaclient',
+}
+
+
 class BaseConfig:
     """Base configuration."""
     DEBUG = False
