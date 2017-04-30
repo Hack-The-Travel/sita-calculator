f723ba3b5888652db88b54fc45be5e90492e4ad7
Sergey Popinevskiy <gurza000@gmail.com>
2017-04-30 18:23:20 +0300

    Implemented 'get ticket price' handler

--- app/ticket/routes.py
+++ app/ticket/routes.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 from flask import render_template
 from . import ticket_blueprint
+from config import sita as sita_config
+from sitaclient import SitaClient
 
 
 @ticket_blueprint.route('/ticket/<ticket_number>', methods=['GET'])
 def ticket_price(ticket_number):
-    context = dict()
+    sita = SitaClient(
+        pos=sita_config['pos'],
+        dumps_dir=sita_config['dumps_dir'] if 'dumps_dir' in sita_config else None
+    )
+    price = sita.get_ticket_price(ticket_number)
+    context = {
+        'ticket_number': ticket_number,
+        'total': price['total'],
+        'fare': price['fare'],
+        'taxes': price['taxes'],
+    }
     return render_template('ticket.html', **context)
