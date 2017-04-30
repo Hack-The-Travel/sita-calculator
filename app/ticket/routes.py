5ddd61f77817a0a29674238045575d1bf1148d8c
Sergey Popinevskiy <gurza000@gmail.com>
2017-04-30 22:19:49 +0300

    Implemented 'ticket price' parser

--- app/ticket/routes.py
+++ app/ticket/routes.py
@@ -5,13 +5,33 @@ from config import sita as sita_config
 from sitaclient import SitaClient
 
 
+def get_ticket_price(self, ticket_response):
+    ns = ticket_response.nsmap
+    if None in ns:
+        ns['ns'] = ns.pop(None)
+    fare_breakdown = ticket_response.find('.//fares:PassengerFare', namespaces=ns)
+    taxes_breakdown = fare_breakdown.findall('fares:Taxes/fares:Tax', namespaces=ns)
+    taxes = list()
+    for tax in taxes_breakdown:
+        taxes.append({
+            'code': tax.attrib['TaxCode'],
+            'amount': tax.attrib['Amount'],
+        })
+    return {
+        'total': fare_breakdown.find('fares:TotalFare', namespaces=ns).attrib['Amount'],
+        'fare': fare_breakdown.find('fares:BaseFare', namespaces=ns).attrib['Amount'],
+        'taxes': taxes,
+    }
+
+
 @ticket_blueprint.route('/ticket/<ticket_number>', methods=['GET'])
 def ticket_price(ticket_number):
     sita = SitaClient(
         pos=sita_config['pos'],
         dumps_dir=sita_config['dumps_dir'] if 'dumps_dir' in sita_config else None
     )
-    price = sita.get_ticket_price(ticket_number)
+    ticket_response = sita.read_ticket(ticket_number)
+    price = get_ticket_price(ticket_response)
     context = {
         'ticket_number': ticket_number,
         'total': price['total'],
